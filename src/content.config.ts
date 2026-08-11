c4d2d5c76487fd56f1e47f6f15f401c1b483156a
CSNZC <casfzea@gmail.com>
2026-08-11 16:21:20 +0800

    整合0811-1

---
+++ src/content.config.ts
@@ -0,0 +1,34 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const news = defineCollection({
+  loader: glob({
+    pattern: "**/*.md",
+    base: "./src/content/news",
+  }),
+
+  schema: z.object({
+    title: z.string(),
+    date: z.coerce.date(),
+    tags: z.array(z.string()).default([]),
+    summary: z.string(),
+  }),
+});
+
+const carousel = defineCollection({
+  loader: glob({
+    pattern: "**/*.{md,json}",
+    base: "./src/content/carousel",
+  }),
+
+  schema: z.object({
+    title: z.string(),
+    image: z.string(),
+    link: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  news,
+  carousel,
+};
